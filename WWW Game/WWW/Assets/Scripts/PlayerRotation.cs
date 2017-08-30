6e3cdfe98429e43f2dae49dbf4190890a2bcff64
Haervwe <31465508+Haervwe@users.noreply.github.com>
2017-08-30 02:44:07 -0300

    Gaem WWarlocks
    420

---
+++ WWW Game/WWW/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+
+    Camera cam;
+    Transform my;
+    Rigidbody2D body;
+    
+    
+  
+
+    void Awake()
+    {
+        cam = Camera.main;
+        my = GetComponent<Transform>();
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+
+    void Update()
+    {
+        // Distance from camera to object.  We need this to get the proper calculation.
+        float camDis = cam.transform.position.y - my.position.y;
+
+        // Get the mouse position in world space. Using camDis for the Z axis.
+        Vector3 mouse = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
+
+        float AngleRad = Mathf.Atan2(mouse.y - my.position.y, mouse.x - my.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+
+        body.rotation = angle-90;
+       
+    }
+}
---
+++ WWW Game/WWW/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject projectile;
+
+    public float visualCd;
+    public float cd = 5f;
+    public string key;
+    float cdInt = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetButtonUp (key) && cdInt <= 0 )
+        {
+            cdInt = cd;
+            Instantiate(projectile, transform.position, transform.rotation);
+
+        }
+        cdInt -= Time.deltaTime;
+        visualCd = cdInt;
+	}
+}
